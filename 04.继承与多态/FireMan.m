b4f035d0d2e7bdc2f43a2ce83af7a8f6967669f4
damys <damys@foxmail.com>
2016-08-15 14:12:07 +0800

    [add] FireMan.m

---
+++ 04.继承与多态/FireMan.m
@@ -0,0 +1,17 @@
+//
+//  FireMan.m
+//  oc base
+//
+//  Created by damys on 16/7/24.
+//  Copyright © 2016年 damys. All rights reserved.
+//
+
+#import "FireMan.h"
+
+@implementation FireMan
+
+- (void)help{
+    NSLog(@"FireMan help...");
+}
+
+@end
