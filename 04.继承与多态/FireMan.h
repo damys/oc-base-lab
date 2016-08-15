455265e88845c16e78ead3b886d64df42be9ccc5
damys <damys@foxmail.com>
2016-08-15 14:06:26 +0800

    [add] FireMan.h

---
+++ 04.继承与多态/FireMan.h
@@ -0,0 +1,13 @@
+//
+//  FireMan.h
+//  oc base
+//
+//  Created by damys on 16/7/24.
+//  Copyright © 2016年 damys. All rights reserved.
+//
+
+#import "Person.h"
+
+@interface FireMan : Person
+
+@end
