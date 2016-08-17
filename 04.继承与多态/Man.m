a3ec6f3f20be3e33d325e44c67f8b2f167252e13
damys <damys@foxmail.com>
2016-08-17 10:33:22 +0800

    [add] Man.m

---
+++ 04.继承与多态/Man.m
@@ -0,0 +1,17 @@
+//
+//  Man.m
+//  oc base
+//
+//  Created by damys on 16/7/24.
+//  Copyright © 2016年 damys. All rights reserved.
+//
+
+#import "Man.h"
+
+@implementation Man
+
+- (void)help{
+    NSLog(@"man help...");
+}
+
+@end
