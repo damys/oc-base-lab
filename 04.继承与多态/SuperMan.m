06d145b0ba800bc0659241e28e544f19171529a5
damys <damys@foxmail.com>
2016-08-19 09:00:53 +0800

    [add] superMan.m

---
+++ 04.继承与多态/SuperMan.m
@@ -0,0 +1,17 @@
+//
+//  SuperMan.m
+//  oc base
+//
+//  Created by damys on 16/7/24.
+//  Copyright © 2016年 damys. All rights reserved.
+//
+
+#import "SuperMan.h"
+
+@implementation SuperMan
+
+- (void)help{
+    NSLog(@"SuperMan help...");
+}
+
+@end
