dad0fff5fde8c16f1e33935f2d80b10fdc5f4804
damys <damys@foxmail.com>
2016-08-25 09:08:02 +0800

    [add] person.m

---
+++ 05.oc特有的语法/Person.m
@@ -0,0 +1,28 @@
+
+#import "Person.h"
+
+@implementation Person
+
+////名子
+//- (void)setName: (NSString *)name{
+//    _name = name;
+//}
+//- (NSString *)name{
+//    return _name;
+//}
+
+
+//say
+- (void)say{
+    NSLog(@"help...");
+}
+
++ (void)hi{
+    NSLog(@"hi...");
+}
+
++ (id)person{
+    //return [Person new]; //写死,创建都是Person
+    return [self new];     //不能写死,这样创建就是本身
+}
+@end
