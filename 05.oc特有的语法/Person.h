c2de511ea72c14a1d71480173fd51de79a743401
damys <damys@foxmail.com>
2016-08-25 09:06:30 +0800

    [add] person.h

---
+++ 05.oc特有的语法/Person.h
@@ -0,0 +1,19 @@
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject{
+    
+}
+@property NSString *name;
+////名子
+//- (void)setName: (NSString *)name;
+//- (NSString *)name;
+
+//say
+- (void)say;
+
++ (void)hi;
+
++ (void)person;
+
+@end
