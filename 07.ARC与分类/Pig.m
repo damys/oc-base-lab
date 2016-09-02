53d881907723f766a0fa877b6be89083913833b7
damys <damys@foxmail.com>
2016-09-02 09:08:00 +0800

    [add] pig.m

---
+++ 07.ARC与分类/Pig.m
@@ -0,0 +1,37 @@
+//
+//  Pig.m
+//  oc base
+//
+//  Created by damys on 16/7/30.
+//  Copyright © 2016年 damys. All rights reserved.
+//
+
+#import "Pig.h"
+
+@implementation Pig
+
+- (void)dealloc{
+    NSLog(@"猪猪死了");
+    [_name release];
+    [super dealloc];
+}
+
+- (instancetype)initWithName:(NSString *)name andAge:(int)age andWeight:(float)weight{
+    if(self = [super init]){
+        _name = name;
+        _age = age;
+        _weight = weight;
+    }
+    return self;
+}
+
++ (instancetype)pig{
+    return [[[self alloc] init] autorelease];
+}
+
+
++ (instancetype)pigWithName:(NSString *)name andAge:(int)age andWeight:(float)weight{
+    return [[[self alloc] initWithName:name andAge:age andWeight:weight] autorelease];
+}
+ 
+@end
