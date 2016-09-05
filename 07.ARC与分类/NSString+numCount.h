fb6d7b65dad0f7d434ff8d05b8c8cc32828048dd
damys <damys@foxmail.com>
2016-09-05 10:13:46 +0800

    [add] NSNstring+numCount.h

---
+++ 07.ARC与分类/NSString+numCount.h
@@ -0,0 +1,13 @@
+//
+//  NSString+numCount.h
+//  oc base
+//
+//  Created by damys on 16/7/31.
+//  Copyright © 2016年 damys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (numCount)
+- (int)numberCount;    //不用传参数, 因为自身就是字符串
+@end
