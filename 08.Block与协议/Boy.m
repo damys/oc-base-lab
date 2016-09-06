954e998cd49f1c231053bc90b5502c56ce5dc817
damys <damys@foxmail.com>
2016-09-06 09:08:48 +0800

    [add] boy.m

---
+++ 08.Block与协议/Boy.m
@@ -0,0 +1,15 @@
+
+#import "Boy.h"
+
+
+@implementation Boy
+
+//谈恋爱的方法
+- (void)talkLove{
+    NSLog(@"talkLove...begin");
+    [_girlFriend wash];
+    [_girlFriend cook];
+    NSLog(@"talkLove...end");
+}
+
+@end
