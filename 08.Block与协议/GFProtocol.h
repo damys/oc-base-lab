7721c0cb287d7817262c7aed1a7c1cfbeb0ca1ea
damys <damys@foxmail.com>
2016-09-06 09:13:43 +0800

    Update GFProtocol.h

--- 08.Block与协议/GFProtocol.h
+++ 08.Block与协议/GFProtocol.h
@@ -1,10 +1,3 @@
-//
-//  GFProtocol.h
-//  oc base
-//
-//  Created by damys on 16/8/2.
-//  Copyright © 2016年 damys. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 //女朋友协议,只要遵守这个协议的东西都可以作为男孩子的女朋友.
