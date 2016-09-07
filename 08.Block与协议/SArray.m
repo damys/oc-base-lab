7cbeb0ea54d7a375842464b586f5e1d4fde7c3bf
damys <damys@foxmail.com>
2016-09-07 09:21:52 +0800

    [add] sarray.m

---
+++ 08.Block与协议/SArray.m
@@ -0,0 +1,47 @@
+//
+//  SArray.m
+//  oc base
+//
+//  Created by damys on 16/8/2.
+//  Copyright © 2016年 damys. All rights reserved.
+//
+
+#import "SArray.h"
+#import <string.h>
+
+@implementation SArray
+//传统方法实现排序
+//- (void)sortWithCountries:(char *[])countries andLength:(int)len{
+//    
+//    for (int i=0; i<len; i++) {
+//        for(int j=0; j<len-1-i; j++){
+//            int res = strcmp(countries[j], countries[j+1]);
+//            if(res>0){
+//                char *temp = countries[j];
+//                countries[j]=countries[j+1];
+//                countries[j+1]=temp;
+//            }
+//        }
+//    }
+//    
+//}
+
+//以block 方式实现自定义
+- (void)sortWithCountries:(char *[])countries andLength:(int)len andCompareBlock:(NewType2)compareBlock{
+
+    for (int i=0; i<len; i++) {
+        for(int j=0; j<len-1-i; j++){
+            
+            //返回YES,NO
+            BOOL res = compareBlock(countries[j], countries[j+1]);
+            
+            if(res == YES){
+                char *temp = countries[j];
+                countries[j]=countries[j+1];
+                countries[j+1]=temp;
+            }
+        }
+    }
+
+}
+@end
