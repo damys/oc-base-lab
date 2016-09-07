bc3f47522e94b572966bc45bf3e29427334f745e
damys <damys@foxmail.com>
2016-09-07 09:24:10 +0800

    Update SArray.h

--- 08.Block与协议/SArray.h
+++ 08.Block与协议/SArray.h
@@ -1,10 +1,3 @@
-//
-//  SArray.h
-//  oc base
-//
-//  Created by damys on 16/8/2.
-//  Copyright © 2016年 damys. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 typedef BOOL (^NewType2)(char *country1, char *country2);
